7822a7dc1445dfab2946c93c8f473f44aab5846a
Kanbagoly <kanbagoly@gmail.com>
2019-10-02 00:34:23 +0100

    Add pom file

---
+++ src/test/java/com/kanbagoly/guice/cached/Test1.java
@@ -0,0 +1,19 @@
+package com.kanbagoly.guice.cached;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+
+    @Test
+    void test() {
+
+
+    }
+
+    static class TestClass {
+
+
+
+    }
+
+}
